3fc92a528b9c6fff7b50718265bc13a9d9d692b8
Haddy <classichaddy@gmail.com>
2025-01-21 13:13:57 +0100

    Automatic configuration with django-heroku

--- art/urls.py
+++ art/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.artdata_search, name='artdata-search2'),
     path('filter/', views.ArtDataListView.as_view()),
     path('import/', views.upload_csv),
     path('search/', views.artdata_search, name='artdata-search'),
