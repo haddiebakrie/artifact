6771fe79501e1276915300008e110a624277e1e5
Haddy <classichaddy@gmail.com>
2025-02-04 17:34:53 +0100

    switch to debug

--- artifact/settings.py
+++ artifact/settings.py
@@ -113,11 +113,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+DEBUG = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
